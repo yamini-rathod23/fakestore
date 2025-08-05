201471887848d32b6685fe75ed25019898ad5321
Yamini Rathod <yrathod@academian.com>
2025-08-05 13:22:07 +0530

    fix

---
+++ src/test/resources/features/karate-config.js
@@ -0,0 +1,9 @@
+function fn() {
+  var config = {};
+  config.headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/115.0 Safari/537.36',
+    'Accept': '*/*',
+    'Content-Type': 'application/json'
+  };
+  return config;
+}
